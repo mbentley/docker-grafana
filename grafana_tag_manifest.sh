4fa8bc3a8b6a92ce8ae429fe6b968609a37617ab
Matt Bentley <mbentley@mbentley.net>
2024-03-06 14:06:50 -0500

    Added 10.4.x
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- grafana_tag_manifest.sh
+++ grafana_tag_manifest.sh
@@ -3,7 +3,7 @@
 set -e
 
 # set expected major.minor tags
-EXPECTED_TAGS="9.4 9.5 10.0 10.1 10.2 10.3"
+EXPECTED_TAGS="9.5 10.0 10.1 10.2 10.3 10.4"
 
 tag_manifest() {
   # get expected tag from first argument
